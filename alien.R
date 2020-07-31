61de5e654989448a7138f510b959f9ae8f86cac6
George Yu <georgejcyu@Georges-MacBook-Pro-3.local>
2020-07-30 17:27:12 -0700

    Message to aliens

---
+++ alien.R
@@ -0,0 +1,7 @@
+cat("Greetings, friends! We are humbled and honoured to be meeting with you.\n
+Our species is known as humans, and we come from planet Earth. We want to let \n
+you know that we come in peace. Our goal is for us to exchange information \n
+with one another, and hope to learn more about each other. We want to develop \n
+a bond with you, and are excited to get to know you. Thank you for your time, \n
+and we hope to hear from you soon!")
+
